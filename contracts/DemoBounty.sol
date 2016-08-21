6b6b6c737e4ed4e16bc34d8cde2660fa931588b4
Makoto Inoue <inouemak@googlemail.com>
2016-08-21 20:23:02 +0100

    Add DemoBounty as a copy of Bounty

---
+++ contracts/DemoBounty.sol
@@ -0,0 +1,53 @@
+import './Conference.sol';
+import './zeppelin/PullPaymentCapable.sol';
+import './zeppelin/Rejector.sol';
+
+/*
+ * Bounty
+ * This bounty will pay out if you can cause a Conference's balance
+ * to be lower from its totalBalance, which would mean that it doesn't
+ * have sufficient ether for everyone to withdraw.
+ */
+contract Bounty is PullPaymentCapable, Rejector{
+  uint public totalBounty;
+  bool public claimed;
+  mapping(address => address) public researchers;
+  event TargetCreation(address createdAddress);
+  event Error(string message);
+
+  function contribute() {
+    totalBounty = msg.value;
+    if (claimed) throw;
+  }
+
+  function createTarget() returns(Conference) {
+    Conference target = new Conference();
+    researchers[target] = msg.sender;
+    TargetCreation(target);
+    return target;
+  }
+
+  modifier hasResearcher(Conference target) {
+    address researcher = researchers[target];
+    if (researcher != 0){
+      _
+    }else{
+      Error('there are no researcher');
+    }
+  }
+
+  modifier hasBug(Conference target) {
+    if (target.totalBalance() > target.balance){
+      _
+    }else{
+      Error('No security breach');
+    }
+  }
+
+  function claim(Conference target) hasResearcher(target) hasBug(target){
+    address researcher = researchers[target];
+    asyncSend(researcher, this.balance);
+    totalBounty = 0;
+    claimed = true;
+  }
+}
