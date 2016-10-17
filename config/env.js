8dc3fc15e71a2dbd5fddcd41b7b8c3a188fa27c3
Jeff Lau <jeff-lau@live.com>
2016-10-17 14:19:42 +0800

    Added NETWORK env variables

--- config/env.js
+++ config/env.js
@@ -16,6 +16,9 @@ function getClientEnvironment(publicUrl) {
       'process.env.NODE_ENV': JSON.stringify(
         process.env.NODE_ENV || 'development'
       ),
+      'process.env.NETWORK': JSON.stringify(
+        process.env.NETWORK || 'testrpc'
+      ),
       // Useful for resolving the correct path to static assets in `public`.
       // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
       // This should only be used as an escape hatch. Normally you would put
