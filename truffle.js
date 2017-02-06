a4542201252d4f90f0bc992737e91a1a1c4c87b7
Makoto Inoue <inouemak@googlemail.com>
2017-02-06 06:42:29 +0000

    Add gas and deploy account

--- truffle.js
+++ truffle.js
@@ -14,7 +14,9 @@ module.exports = {
     mainnet: {
       host: "localhost",
       port: 8545,
-      network_id: 1
+      network_id: 1,
+      gas: 1990000,
+      from: '0xFa6F2D7cC987d592556ac07392b9d6395bfcc379'
     }
   }
 };
