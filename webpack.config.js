d2eb6a0d65d19967884d5d33abb5b9c154deeef7
Makoto Inoue <makoto.inoue@simplybusiness.co.uk>
2017-03-01 19:06:04 +0000

    use web pack.config.js provided from web pack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,36 @@
-var fs = require("fs");
-var path = require('path');
-var webpack = require("webpack");
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var environment = process.env.NODE_ENV || "development";
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var provided = {
-  "Web3": "web3",
-  "Pudding": "ether-pudding",
-  "Promise": "bluebird"
-};
-
-// Get all the compiled contracts for our environment.
-var contracts_directory = "./build/contracts";
-fs.readdirSync("./build/contracts").forEach(function(file) {
-  if (path.basename(file).indexOf(".sol.js")) {
-    provided[path.basename(file, ".sol.js")] = path.resolve(contracts_directory + "/" + file);
-  }
-});
-providedPlugins = new webpack.ProvidePlugin(provided)
-console.log('**providedPlugins', providedPlugins)
 module.exports = {
   entry: './app/javascripts/app.js',
   output: {
-    path: "./build",
+    path: path.resolve(__dirname, 'build'),
     filename: 'app.js'
   },
-  devtool:'#source-map',
-  module: {
-    loaders: [
-      { test: /\.(js|jsx|es6)$/, exclude: /node_modules/, loader: "babel-loader"},
-      { test: /\.scss$/i, loader: ExtractTextPlugin.extract(["css", "sass"])},
-      { test: /\.css/, loader: "style-loader!css-loader" },
-      { test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' },
-      { test: /\.json$/i, loader: "json"}
-    ]
-  },
   plugins: [
-    new webpack.DefinePlugin({
-        ENV: '"' + process.env.NODE_ENV + '"',
-        WEB3_PROVIDER_LOCATION: '"' + process.env.WEB3_PROVIDER_LOCATION + '"'
-    }),
-    providedPlugins,
+    // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" },
-      { from: './app/images', to: "images" },
-    ]),
-    new ExtractTextPlugin("app.css")
+      { from: './app/index.html', to: "index.html" }
+    ])
   ],
-  resolve: { fallback: path.join(__dirname, "node_modules") },
-  resolveLoader: { fallback: path.join(__dirname, "node_modules") }
-};
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
