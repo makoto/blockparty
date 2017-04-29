10d6d172caf4e589c298acc74006c2b760a64b0a
Makoto Inoue <inouemak@googlemail.com>
2017-04-29 15:07:39 +0100

    Make migration configurable

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,17 @@
 var Conference = artifacts.require("./Conference.sol");
 var InvitationRepository = artifacts.require("./InvitationRepository.sol");
 var coolingPeriod = 1 * 60 * 60 * 24 * 7;
-module.exports = function(deployer) {
-  deployer.deploy(InvitationRepository).then(function() {
-    return deployer.deploy(Conference, coolingPeriod, InvitationRepository.address);
-  });
+var invitationAddress = 0;
+// this is already required by truffle;
+var yargs = require('yargs');
+
+// eg:  truffle migrate --config '{"invitation":true}'
+var config = JSON.parse(yargs.argv.config);
+
+module.exports = async function(deployer) {
+  if (config.invitation) {
+    await deployer.deploy(InvitationRepository);
+    invitationAddress = InvitationRepository.address;
+  }
+  return deployer.deploy(Conference, coolingPeriod, invitationAddress);
 };
