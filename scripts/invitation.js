a4c0b8dbe924d6ea428c49c6b7a0f40725f61cb1
Makoto Inoue <inouemak@googlemail.com>
2017-04-30 18:12:44 +0100

    Handle both invitation and confirmation

--- scripts/invitation.js
+++ scripts/invitation.js
@@ -1,25 +1,40 @@
 let fs = require('fs');
 let InvitationRepository = artifacts.require("./InvitationRepository.sol");
+let ConfirmationRepository = artifacts.require("./ConfirmationRepository.sol");
+let repository;
+let arg = require('yargs').argv;
 
-if (process.argv.length < 5) {
-  throw('usage: truffle exec scripts/invitation.js invitation_codes.txt');
+console.log('args', arg);
+if (!(arg.i && arg.t)) {
+  throw('usage: truffle exec scripts/repository.js -t invitation|confirmation -i codes.txt');
 }
-
-let file = process.argv[4];
-let invitation_codes = fs.readFileSync(file, 'utf8').trim().split('\n')
-
 module.exports = async function(callback) {
-  let invitation = await InvitationRepository.deployed();
-  for (var i = 0; i < invitation_codes.length; i++) {
-    var invitation_code = invitation_codes[i];
-    var registered = await invitation.verify(invitation_code);
+  let file = arg.i;
+  console.log('file', file)
+  switch (arg.t) {
+    case 'invitation':
+      repository = await InvitationRepository.deployed();
+      break;
+    case 'confirmation':
+      repository = await ConfirmationRepository.deployed();
+      break;
+    default:
+      throw('-t must be either invitation or confirmation');
+  }
+  console.log(2)
+  let codes = fs.readFileSync(file, 'utf8').trim().split('\n');
+
+  for (var i = 0; i < codes.length; i++) {
+    console.log('code', codes[i])
+    var code = codes[i];
+    var registered = await repository.verify(code);
     if (registered) {
-      var claimed = await invitation.report(invitation_code);
-      console.log('invitation_code', invitation_code, ' is already registered. Claimed by ', claimed);
+      var claimed = await repository.report(code);
+      console.log('code', code, ' is already registered. Claimed by ', claimed);
     }else{
-      var encrypted_code = await invitation.encrypt.call(invitation_code);
-      console.log('Adding', invitation_code, ' as ', encrypted_code);
-      await invitation.add([encrypted_code]).catch(function(){});
+      var encrypted_code = await repository.encrypt.call(code);
+      console.log('Adding', code, ' as ', encrypted_code);
+      await repository.add([encrypted_code]).catch(function(){});
     }
   }
 }
