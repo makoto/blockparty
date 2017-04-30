2e546a51debb668cc238c63a796a872508e13a68
Makoto Inoue <inouemak@googlemail.com>
2017-04-30 20:25:34 +0100

    Fix broken migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,15 +12,30 @@ var yargs = require('yargs');
 if (yargs.argv.config) {
   var config = JSON.parse(yargs.argv.config);
 }
-console.log('config', config);
-module.exports = async function(deployer) {
-  if (deployer.network == 'test' || config.invitation) {
-    await deployer.deploy(InvitationRepository);
-    invitationAddress = InvitationRepository.address;
-  }
-  if (deployer.network == 'test' || config.confirmation) {
-    await deployer.deploy(ConfirmationRepository);
-    confirmationAddress = ConfirmationRepository.address;
-  }
-  return deployer.deploy(Conference, coolingPeriod, invitationAddress, confirmationAddress);
+// This does not seem to write all contract address into build/contracts/*json
+// module.exports = async function(deployer) {
+//   if (deployer.network == 'test' || config.invitation) {
+//     await deployer.deploy(InvitationRepository);
+//     invitationAddress = InvitationRepository.address;
+//   }
+//   if (deployer.network == 'test' || config.confirmation) {
+//     await deployer.deploy(ConfirmationRepository);
+//     confirmationAddress = ConfirmationRepository.address;
+//   }
+//   console.log('Deployment configuration', coolingPeriod, invitationAddress, confirmationAddress);
+//   return deployer.deploy(Conference, coolingPeriod, invitationAddress, confirmationAddress);
+// };
+
+module.exports = function(deployer) {
+  deployer.deploy(InvitationRepository).then(function() {
+    if (deployer.network == 'test' || config.invitation) {
+      invitationAddress = InvitationRepository.address;
+    }
+    return deployer.deploy(ConfirmationRepository);
+  }).then(function(){
+    if (deployer.network == 'test' || config.confirmation) {
+      confirmationAddress = ConfirmationRepository.address;
+    }
+    return deployer.deploy(Conference, coolingPeriod, invitationAddress, confirmationAddress);
+  });
 };
