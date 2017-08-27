3a9dd1a0bb44b2982c58bd346a4272221a4b0e70
Makoto Inoue <inouemak@googlemail.com>
2017-08-27 16:27:12 +0100

    Changed the way to find event data based on address specified in the deployed contract.

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,8 +4,24 @@ import React from 'react';
 
 let Data = [
   {
-    name: 'CodeUp #21',
+    name: 'September Ethereum London Meetup at Imperial College',
     address: null,
+    date:'Wednesday, September 13, 2017 6:45 PM to 9:00 PM(UTC+1)',
+    map_url: 'https://maps.google.com/maps?f=q&hl=en&q=Huxley+Building%2C+Imperial+College+%2C+London+SW7+2AZ%2C+gb',
+    location_text: 'Clore Lecture Theatre, Huxley Building, Imperial College, London (SW7 2AZ)',
+    description_text: ''
+  },
+  {
+    name: 'CodeUp #22',
+    address: null,
+    date:'Sep 7th (Thur) 6:30 PM(UTC+1)',
+    map_url: 'https://www.google.co.uk/maps/place/Simply+Business/@51.51528,-0.0904011,15z/data=!4m5!3m4!1s0x0:0x70f23f24baccffdc!8m2!3d51.51528!4d-0.0904011',
+    location_text: 'Simply Business (99 Gresham St, London EC2V 7NG)',
+    description_text: ''
+  },
+  {
+    name: 'CodeUp #21',
+    address: '0x97a6ec1ea5810388b6cb513b087e55a65c4bf417',
     date:'Jun 22nd (Thur) 6:30 PM(UTC+1)',
     map_url: 'https://www.google.co.uk/maps/place/Simply+Business/@51.51528,-0.0904011,15z/data=!4m5!3m4!1s0x0:0x70f23f24baccffdc!8m2!3d51.51528!4d-0.0904011',
     location_text: 'Simply Business (99 Gresham St, London EC2V 7NG)',
@@ -13,9 +29,7 @@ let Data = [
   },
   {
     name: 'Edcon Post conference lunch',
-    // main:
-    // ropsten: 0x2a18b238aed745f923f954db65f7db45a4c41079
-    address: null,
+    address: '0x7b249881af36cccd1ab2e4325a8eed2a7848b263',
     date:'Feb 19th (Sun) 12:30(UTC+1)',
     map_url: 'https://www.google.co.uk/maps/place/Mama+Shelter+Paris/@48.8597157,2.4006691,17z/data=!4m5!3m4!1s0x47e66d8670ddcb91:0xb81d5c1e065c8167!8m2!3d48.8597122!4d2.4028631',
     location_text: 'Mama Shelter Paris (109 Rue de Bagnolet, 75020 Paris)',
