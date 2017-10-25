5f8d2b196340c1806e7437c99f20e28751234311
Makoto Inoue <inouemak@googlemail.com>
2017-10-25 20:28:41 +0100

    Reset app_config

--- app_config.js
+++ app_config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   ropsten: {
     contract_addresses:{
-      'Conference': '0xf5397b6ac55e74da9a312eeca3b59a8bd8ba976b',
+      'Conference': null,
       'ConfirmationRepository': '0xe444b7bec7a2dbd571523a1e2e4580a1b4c1c9b1'
     },
     name: 'ROPSTEN NET',
@@ -20,7 +20,7 @@ module.exports = {
   },
   mainnet: {
     contract_addresses:{
-      'Conference': '0xf618af176f6521ae0ab249133740dd173c3e43e1',
+      'Conference': null,
       'ConfirmationRepository': '0xe0e0229484b1088e0a751ddffd05b2e6b833e3a2'
     },
     name: 'MAINNET',
