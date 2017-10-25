0d034e2abca5551fed896d0a9f8fd4593d2e9f6b
Makoto Inoue <inouemak@googlemail.com>
2017-10-25 20:28:50 +0100

    Fix Simulation.js

--- scripts/simulation.js
+++ scripts/simulation.js
@@ -1,4 +1,4 @@
-// truffle migrate --config '{"invitation":false, "confirmation":false}' --reset
+// truffle migrate --config '{"name":"Test", "confirmation":false}' --network development
 // for i in {1..100}; do echo $i; done > simulation.txt
 let fs = require('fs');
 let Conference = artifacts.require("./Conference.sol");
@@ -11,17 +11,17 @@ if (!(arg.i)) {
 module.exports = async function(callback) {
   let file = arg.i;
   console.log('file', file)
-  repository = await ConfirmationRepository.deployed();
+  // repository = await ConfirmationRepository.deployed();
   conference = await Conference.deployed();
   let codes = fs.readFileSync(file, 'utf8').trim().split('\n');
   await conference.setLimitOfParticipants(codes.length + 10, {from:web3.eth.accounts[0]});
 
   for (var i = 0; i < codes.length; i++) {
     var code = codes[i];
-    var registered = await repository.verify(code);
-    var encrypted_code = await repository.encrypt.call(code);
-    var deposit = web3.toWei(0.1, "ether");
-    await repository.add([encrypted_code]).catch(function(){});
+    // var registered = await repository.verify(code);
+    // var encrypted_code = await repository.encrypt.call(code);
+    var deposit = await conference.deposit.call();
+    // await repository.add([encrypted_code]).catch(function(){});
     console.log('registering ', codes[i] , ' as ', web3.eth.accounts[i])
     await conference.register('user' + codes[i], {from:web3.eth.accounts[i], value:deposit})
     var registered = await conference.registered.call();
