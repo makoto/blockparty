8e94db421e4800d58589a9f04c993ea2d175a8e8
Makoto <makoto@Makotos-Air.lan>
2018-03-20 19:20:22 +0000

    Change group admin

--- contracts/GroupAdmin.sol
+++ contracts/GroupAdmin.sol
@@ -8,24 +8,50 @@ import './zeppelin/ownership/Ownable.sol';
 contract GroupAdmin is Ownable {
   event AdminGranted(address indexed grantee);
   event AdminRevoked(address indexed grantee);
-  mapping(address=>bool) internal admins;
+  address[] public admins;
 
   modifier onlyAdmin() {
-    require(admins[msg.sender] || msg.sender == owner);
+    require(isAdmin(msg.sender));
     _;
   }
 
-  function grant(address newAdmin) public onlyOwner{
-    admins[newAdmin] = true;
-    AdminGranted(newAdmin);
+  function grant(address[] newAdmins) public onlyOwner{
+    for(uint i=0;i<newAdmins.length;i++){
+      admins.push(newAdmins[i]);
+      AdminGranted(newAdmins[i]);
+    }
   }
 
-  function revoke(address oldAdmin) public onlyOwner{
-    admins[oldAdmin] = false;
-    AdminRevoked(oldAdmin);
+  function revoke(address[] oldAdmins) public onlyOwner{
+    for(uint i=0;i<oldAdmins.length;i++){
+        for (uint j = 0; j < admins.length; ++i) {
+            if (admins[j] == oldAdmins[i]) {
+                admins[j] = admins[admins.length - 1];
+                admins.length--;
+                AdminRevoked(oldAdmins[i]);
+                break;
+            }
+        }
+    }
   }
+
+  function getAdmins() view public returns(address[]){
+    // todo: include owner;
+    return admins;
+  }
+
+  function numOfAdmins() view public returns(uint){
+      return admins.length;
+  }
+
 
   function isAdmin(address admin) public view returns(bool){
-    return(admins[admin] || admin == owner);
+    if (admin == owner) return true;
+    for(uint i=0;i<admins.length;i++){
+      if(admins[i] == admin){
+        return true;
+      }
+    }
+    return false;
   }
 }
