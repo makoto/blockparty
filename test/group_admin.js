0fdbbf161bb598fd44be2b63cb53f77962b85a98
Makoto <makoto@Makotos-Air.lan>
2018-03-20 19:19:59 +0000

    Change admins from hash to array

--- test/group_admin.js
+++ test/group_admin.js
@@ -1,5 +1,4 @@
 const GroupAdmin = artifacts.require("GroupAdmin.sol");
-
 contract('GrupAdmin', function(accounts) {
     let admin, operator, owner;
     beforeEach(async function(){
@@ -17,31 +16,39 @@ contract('GrupAdmin', function(accounts) {
     
     describe('on grant', function(){
         it('is added to admin', async function(){
-            await admin.grant(operator, {from:owner});
+            await admin.grant([operator], {from:owner});
             assert.strictEqual(await admin.isAdmin.call(operator), true);
             assert.strictEqual(await admin.isAdmin.call(non_operator), false);
         })
 
         it('cannot be added by non owner', async function(){
-            await admin.grant(operator, {from:operator}).catch(function(){});
+            await admin.grant([operator], {from:operator}).catch(function(){});
             assert.strictEqual(await admin.isAdmin.call(operator), false);
         })
     })
 
     describe('on revoke', function(){
         beforeEach(async function(){
-            await admin.grant(operator, {from:owner});
+            await admin.grant([operator], {from:owner});
             assert.strictEqual(await admin.isAdmin.call(operator), true);
         })
     
         it('is revoked from admin', async function(){
-            await admin.revoke(operator, {from:owner});
+            await admin.revoke([operator], {from:owner});
             assert.strictEqual(await admin.isAdmin.call(operator), false);
         })
 
         it('cannot be revoked by non owner', async function(){
-            await admin.revoke(operator, {from:operator}).catch(function(){});
+            await admin.revoke([operator], {from:operator}).catch(function(){});
             assert.strictEqual(await admin.isAdmin.call(operator), true);
+        })
+    })
+
+    describe('admins', async function(){
+        it('list number of admins', async function(){
+            await admin.grant([operator], {from:owner})
+            await admin.grant([non_operator], {from:owner})
+            console.log('admin', await admin.getAdmins.call());
         })
     })
 })
