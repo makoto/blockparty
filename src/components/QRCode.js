315e0982bb57933b8582af10b9682ec9000e587c
Makoto <makoto@Makotos-Air.lan>
2018-04-07 20:40:25 +0100

    Add QR Code

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -3,29 +3,34 @@ import ReactDOM from 'react-dom';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
 
-const styles = {
-  paperRight:{
-    flex: 3,
-    textAlign: 'center',
-  }
-};
-
 class QRCode extends React.Component {
   constructor(props) {
     super(props);
   }
 
   handleSearchField(event){
-    this.props.eventEmitter.emit('search', 'makoto')
+    web3.currentProvider
+      .scanQRCode()
+      .then(data => {
+        console.log('QR Scanned:', data)
+        this.props.eventEmitter.emit('search', data)
+      })
+      .catch(err => {
+        console.log('Error:', err)
+      })
   }
 
   render() {
-    return (
-      <IconButton onClick={this.handleSearchField.bind(this)}>
-        {/* from https://materialdesignicons.com */}
-        <Avatar src={require('../images/qrcode-scan.png')} size={26} backgroundColor="white" />
-      </IconButton>
-    );
+    if(web3.currentProvider.scanQRCode){
+      return (
+        <IconButton onClick={this.handleSearchField.bind(this)}>
+          {/* from https://materialdesignicons.com */}
+          <Avatar src={require('../images/qrcode-scan.png')} size={26} backgroundColor="white" />
+        </IconButton>
+      );
+    }else{
+      return false;
+    }
   }
 }
 export default QRCode;
