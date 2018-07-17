5ad1c22d9c08439687623327372402fa840765a9
Makoto Inoue <2630+makoto@users.noreply.github.com>
2018-07-17 11:22:09 +0100

    Enshack (#168)
    * Add data for ENS hack
    
    * Changed description text

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,6 +2,15 @@
 // May be replaced with IPFS at some point.
 let Data = [
   {
+    name:"ENS Hackathon 2018",
+    address:"0x742cd182c2e63b60ca2a13ed3095cccaac69e86c",
+    date:"11-12th(Sat/Sun) August 2018",
+    map_url:"https://goo.gl/maps/ywdQBuEAfns",
+    location_text:"King's college London at Strand London WC2R 2LS",
+    description_text:"To be qualifed for the payout, please PHYSICALLY be at the venue by 10am at the latest",
+    ether_price:453
+  },
+  {
     name:"CodeUp #29",
     address:"0x742cd182c2e63b60ca2a13ed3095cccaac69e86c",
     date:"Thursday, July 19, 2018 6:30 PM to 9:00 PM(UTC+1)",
