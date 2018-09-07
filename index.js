fdeb0a64a255f217e1010d55a15a109826712a40
Ramesh Nair <ram@hiddentao.com>
2018-09-07 16:27:57 +0200

    export Conference too

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const Deployer = require('./build/contracts/Deployer.json')
+const Conference = require('./build/contracts/Conference.json')
 
-module.exports = { Deployer }
+module.exports = { Deployer, Conference }
