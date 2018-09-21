242a1a2a37ef9c47fdbc39997a416cb515e14ff3
Ramesh Nair <ram@hiddentao.com>
2018-09-21 19:23:58 +0100

    export more events

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ module.exports = {
   Deployer,
   Conference,
   events: {
-    /* when new Party gets deployed */
-    NewParty: Deployer.abi.find(({ type, name }) => type === 'event' && name === 'NewParty')
+    /* when new party gets deployed */
+    NewParty: Deployer.abi.find(({ type, name }) => type === 'event' && name === 'NewParty'),
+    /* when someone registers for a party */
+    Register: Conference.abi.find(({ type, name }) => type === 'event' && name === 'RegisterEvent'),
+    /* when someone is marked as having attended a party */
+    Attend: Conference.abi.find(({ type, name }) => type === 'event' && name === 'AttendEvent'),
+    /* when someone withdraws their payout */
+    Withdraw: Conference.abi.find(({ type, name }) => type === 'event' && name === 'WithdrawEvent'),
+    /* when the party gets ended or cancelled */
+    EndParty: Conference.abi.find(({ type, name }) => type === 'event' && (name === 'PaybackEvent' || name === 'CancelEvent')),
+    /* when a new admin gets added */
+    AdminAdded: Conference.abi.find(({ type, name }) => type === 'event' && name === 'AdminGranted'),
+    /* when an admin gets removed */
+    AdminRemoved: Conference.abi.find(({ type, name }) => type === 'event' && name === 'AdminRevoked'),
   }
 }
--- test/exports.js
+++ test/exports.js
@@ -8,5 +8,11 @@ contract('NPM exports', () => {
 
   it('exports valid events', () => {
     assert.isOk(packageExport.events.NewParty.name)
+    assert.isOk(packageExport.events.Register.name)
+    assert.isOk(packageExport.events.Attend.name)
+    assert.isOk(packageExport.events.Withdraw.name)
+    assert.isOk(packageExport.events.EndParty.name)
+    assert.isOk(packageExport.events.AdminAdded.name)
+    assert.isOk(packageExport.events.AdminRemoved.name)
   })
 })
