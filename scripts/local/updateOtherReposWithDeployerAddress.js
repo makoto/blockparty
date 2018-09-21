d2b527d195674c7e68628eb3cefa56d0eb1573e8
Ramesh Nair <ram@hiddentao.com>
2018-09-21 09:07:35 +0100

    revert filename change

--- scripts/local/updateOtherReposWithDeployerAddress.js
+++ scripts/local/updateOtherReposWithDeployerAddress.js
@@ -30,7 +30,7 @@ async function init () {
   if (fs.existsSync(appDir)) {
     console.log('Writing to app config ...')
 
-    const appConfigPath = path.join(appDir, 'src', 'config', 'env.js')
+    const appConfigPath = path.join(appDir, 'src', 'config', 'env.json')
     let appConfig = {}
     try {
       appConfig = require(appConfigPath)
